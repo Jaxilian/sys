b87327324d545973f25d0b635b3fe7e6974d2a76
Jaxilian <jax.carls@gmail.com>
2024-07-24 20:52:48 +0200

    updt

---
+++ ven/requirements.sh
@@ -0,0 +1,20 @@
+sudo apt-get install -y \
+    meson \
+    libwayland-dev \
+    wayland-protocols \
+    libegl1-mesa-dev \
+    libgles2-mesa-dev \
+    libvulkan-dev \
+    vulkan-utils \
+    libdrm-dev \
+    libgbm-dev \
+    libinput-dev \
+    libxkbcommon-dev \
+    libudev-dev \
+    libpixman-1-dev \
+    libseat-dev \
+    hwdata \
+    libdisplay-info-dev \
+    libliftoff-dev
+
+git clone https://gitlab.freedesktop.org/wlroots/wlroots.git
